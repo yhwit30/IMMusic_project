c7e03dc9ef456088ae885bedac9e8eac4ee9aec1
itkmc <150234520+itkmc@users.noreply.github.com>
2024-04-16 17:30:46 +0900

    mainPage css (#66)
    * mainpage video 및 scroll 도입
    
    * main page
    
    * head.jspf 수정
    
    * in-view item 나오는 방식  변경
    
    * main 수정
    
    * mainPage css
    
    ---------
    
    Co-authored-by: itkmc <it0219@gmail.com>

--- src/main/resources/static/resource/main.js
+++ src/main/resources/static/resource/main.js
@@ -1,40 +1,95 @@
-(function() {
-    "use strict";
+$(function(){
 
-    // ".timeline li" 선택자에 해당하는 모든 요소를 찾아서 items 변수에 저장
-    var items = document.querySelectorAll(".timeline li");
 
-    // 뷰포트 내에 요소가 있는지 확인하는 함수
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-            // 요소의 왼쪽 경계가 화면 안에 들어오거나, 요소의 오른쪽 경계가 화면 안에 들어오면 됩니다.
-            (rect.left <= 0 || rect.right >= 0)
-        );
+
+  
+   var header = $('#header'),
+    intrH = $('#intro').innerHeight(),
+    scrollOfSet =  $(window).scrollTop();
+
+    //fiexd header 
+     checkscroll(scrollOfSet)
+
+  $(window).on('scroll', function()  {
+   
+   scrollOfSet = $(this).scrollTop();
+
+   checkscroll(scrollOfSet)
+  });
+
+  function checkscroll(scrollOfSet)
+  {
+
+    if (scrollOfSet >= intrH ) {
+    	header.addClass('fixed');
+    }   else
+    {
+    	header.removeClass('fixed');
     }
+  }
 
-    function callbackFunc() {
-        // 모든 타임라인 항목을 순회
-        for (var i = 0; i < items.length; i++) {
-            // 인덱스에 따라서 해당 항목과 그 다음 항목이 뷰포트에 모두 있을 때 "in-view" 클래스를 추가
-            if (isElementInViewport(items[i]) && isElementInViewport(items[i + 1])) {
-                items[i].classList.add("in-view");
-                items[i + 1].classList.add("in-view");
-            }
-            // 다음 요소로 건너뛰기 위해 인덱스를 증가시킴
-            i++;
-        }
-    }
+   // smoot scroll
+     $("[data-scroll]").on('click', function(event) {
+       event.preventDefault();
 
-    // 페이지 로드 시 함수 실행
-    window.addEventListener("load", callbackFunc);
-    // 뷰포트 크기 변경 시 함수 실행
-    window.addEventListener("resize", callbackFunc);
-    // 스크롤 시 함수 실행
-    window.addEventListener("scroll", callbackFunc);
-})();
+       var $this = $(this),
+          blocktId = $(this).data('scroll'),         
+          blockOfset = $(blocktId).offset().top;
+         
+         $('nav a').removeClass('active'); 
+          $this.addClass('active');
+
+          $("html , body").animate({
+            scrollTop:blockOfset 
+          }, 500);    
+        
+
+
+     });
+   
+     /*nav toogler*/
+     $('#nav__toggle').on('click', function(event){
+             event.preventDefault();
+        $(this).toggleClass("active"); 
+           $('#nav').toggleClass("active");
+ 
+
+     });
+     
+        // collapse
+        $("[data-collapse]").on("click", function(event){
+          event.preventDefault();
+
+          var $this = $(this),
+          blocktId = $(this).data('collapse');
+
+          $this.toggleClass("active");
+          
+
+
+        });
+          //silder
+         $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    $(document).ready(function() {
+
+  $("#form").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+      $("#form").trigger("reset");
+    });
+    return false;
+  });
+  
+});
+
+});
