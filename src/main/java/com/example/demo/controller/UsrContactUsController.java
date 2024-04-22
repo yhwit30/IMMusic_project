dd7ced767772d949a2f4f1f8a31a9b57469975e0
oohyoo <153146836+ohyo555@users.noreply.github.com>
2024-04-22 17:34:42 +0900

    merge 오류 (#85)
    * testteststsetse그만ㅇ!!!
    
    * contactUs jsp 초기작업
    
    * contactUs 폼 구분
    
    * contactUs 구현 중
    
    * contactUs UI - 내용, 색상, 라디오 버튼 수정
    
    * contactUs - 파일 첨부 스타일 커스텀, 기타 UI 수정
    
    * 파일 업로드 UI 수정, 스크롤바 변경
    
    * contactUs UI 1차 구현!
    
    * contactUs concert UI 수정
    
    * contactUs UI 2차 구현 끝
    
    * contactUs 개인정보 체크박스
    
    * top button hover 수정
    
    * application 적용중
    
    * 이메일 select 적용
    
    * gender select변경
    
    * DB 수정
    
    * concert 수정vo 생성
    
    * vo,service,repository 구축
    
    * concert 기능 구현 완료
    
    ---------
    
    Co-authored-by: ohyo555 <555gywn@gamil.com>
    Co-authored-by: yhwit30 <153142837+yhwit30@users.noreply.github.com>

--- src/main/java/com/example/demo/controller/UsrContactUsController.java
+++ src/main/java/com/example/demo/controller/UsrContactUsController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.ContactUsService;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Concert;
+import com.example.demo.vo.ResultData;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -28,19 +30,32 @@ public class UsrContactUsController {
 	public String signupconcert(String name, String emailId, String emailoption, String cellphoneNum, String concertdate,
 			String postcode, String address, String detailAddress, String extraAddress, String inquiry, int check, HttpSession httpSession, HttpServletRequest req) {
 		
-		String fullaemail = emailId + "@" + emailoption;
+		String fullemail = emailId + "@" + emailoption;
 		String fulladdress = address + detailAddress + extraAddress;
 		
-		contactUsService.signup();
+		ResultData<Integer> signupRd = contactUsService.signup(name, fullemail, cellphoneNum, concertdate, postcode, fulladdress, inquiry, check);
 		
-		return "/usr/home/main";
+		return Ut.jsReplace("S-1", signupRd.getMsg(), "../home/main");
 	}
 	
 	@RequestMapping("/usr/contactUs/application")
 	public String contactUsapplication() {
-
+		
 		return "usr/contactUs/application";
 	}
+	
+	@RequestMapping("/usr/contactUs/joinapplication")
+	public String joinapplication(String name, String birth, String gender, String cellphoneNum, String emailId, String emailoption,
+			String postcode, String address, String detailAddress, String extraAddress, String major, String sns, String photo, String career, String introduce, int check, HttpSession httpSession, HttpServletRequest req) {
+		
+		String fullemail = emailId + "@" + emailoption;
+		String fulladdress = address + detailAddress + extraAddress;
+		
+		ResultData<Integer> joinRd = contactUsService.join(name, birth, gender, cellphoneNum, fullemail, postcode, fulladdress, major, sns, introduce, check);
+	
+		return Ut.jsReplace("S-1", joinRd.getMsg(), "../home/main");
+	}
+	
 	
 	@RequestMapping("/usr/contactUs/artist")
 	public String showAboutArtist() {
