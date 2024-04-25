96b3a88d1322b830db8ab3ea830487623caa4ef0
oohyoo <153146836+ohyo555@users.noreply.github.com>
2024-04-25 14:00:27 +0900

    문자 api (#96)
    * testteststsetse그만ㅇ!!!
    
    * contactUs jsp 초기작업
    
    * contactUs 폼 구분
    
    * contactUs 구현 중
    
    * contactUs UI - 내용, 색상, 라디오 버튼 수정
    
    * contactUs - 파일 첨부 스타일 커스텀, 기타 UI 수정
    
    * 파일 업로드 UI 수정, 스크롤바 변경
    
    * contactUs UI 1차 구현!
    
    * contactUs concert UI 수정
    
    * contactUs UI 2차 구현 끝
    
    * contactUs 개인정보 체크박스
    
    * top button hover 수정
    
    * application 적용중
    
    * 이메일 select 적용
    
    * gender select변경
    
    * DB 수정
    
    * concert 수정vo 생성
    
    * vo,service,repository 구축
    
    * concert 기능 구현 완료
    
    * genfile 파일 경로 지정 중
    
    * file업로드를 위한 소스 수정
    
    * 연주자 가입신청 파일업로드 제외 기능 구현 완료
    
    * 개인정보 동의 체크박스 수정
    
    * contactUs form 배경
    
    * DB 수정
    
    * 연주자 가입 신청
    
    * 연주자 가입 신청 파일 업로드 완료
    
    * 문자 api
    
    * 가입 후 문자메시지 전송
    
    ---------
    
    Co-authored-by: ohyo555 <555gywn@gamil.com>
    Co-authored-by: yhwit30 <153142837+yhwit30@users.noreply.github.com>

--- src/main/java/com/example/demo/controller/UsrContactUsController.java
+++ src/main/java/com/example/demo/controller/UsrContactUsController.java
@@ -43,6 +43,8 @@ public class UsrContactUsController {
 		
 		ResultData<Integer> signupRd = contactUsService.signup(name, fullemail, cellphoneNum, concertdate, postcode, fulladdress, inquiry, check);
 		
+		contactUsService.sendsingupMessage(cellphoneNum);
+		
 		return Ut.jsReplace("S-1", signupRd.getMsg(), "../home/main");
 	}
 	
@@ -85,7 +87,9 @@ public class UsrContactUsController {
 		id, relTypeCode); } 
 		
 		}
-				
+		
+		contactUsService.sendjoinMessage(cellphoneNum);
+		
 		return Ut.jsReplace("S-1", joinRd.getMsg(), "../home/main");
 	}
 	
--- src/main/java/com/example/demo/service/ContactUsService.java
+++ src/main/java/com/example/demo/service/ContactUsService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +12,11 @@ import com.example.demo.util.Ut;
 import com.example.demo.vo.Concert;
 import com.example.demo.vo.Member;
 import com.example.demo.vo.ResultData;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 
 @Service
 public class ContactUsService {
@@ -35,6 +44,51 @@ public class ContactUsService {
 
 	public int getCurrentPressId() {
 		return contactUsRepository.getCurrentPressId();
+	}
+
+	public void sendjoinMessage(String cellphoneNum) {
+
+		DefaultMessageService messageService =  NurigoApp.INSTANCE.initialize("API 키 입력", "API 시크릿 키 입력", "https://api.coolsms.co.kr");
+		// Message 패키지가 중복될 경우 net.nurigo.sdk.message.model.Message로 치환하여 주세요
+		Message message = new Message();
+		message.setFrom("발신자번호");
+		message.setTo(cellphoneNum);
+		message.setText("[IMMusic] 연주자 가입신청이 완료되었습니다.");
+		
+		try {
+		  // send 메소드로 ArrayList<Message> 객체를 넣어도 동작합니다!
+		  messageService.send(message);
+		} catch (NurigoMessageNotReceivedException exception) {
+		  // 발송에 실패한 메시지 목록을 확인할 수 있습니다!
+		  System.out.println(exception.getFailedMessageList());
+		  System.out.println(exception.getMessage());
+		} catch (Exception exception) {
+		  System.out.println(exception.getMessage());
+		}
+		
+	}
+
+	public void sendsingupMessage(String cellphoneNum) {
+		
+		DefaultMessageService messageService =  NurigoApp.INSTANCE.initialize("API 키 입력", "API 시크릿 키 입력", "https://api.coolsms.co.kr");
+		// Message 패키지가 중복될 경우 net.nurigo.sdk.message.model.Message로 치환하여 주세요
+		Message message = new Message();
+		message.setFrom("01076070903");
+		message.setTo(cellphoneNum);
+		message.setText("[IMMusic] 연주문의가 완료되었습니다.");
+		
+		try {
+		  // send 메소드로 ArrayList<Message> 객체를 넣어도 동작합니다!
+		  messageService.send(message);
+		} catch (NurigoMessageNotReceivedException exception) {
+		  // 발송에 실패한 메시지 목록을 확인할 수 있습니다!
+		  System.out.println(exception.getFailedMessageList());
+		  System.out.println(exception.getMessage());
+		} catch (Exception exception) {
+		  System.out.println(exception.getMessage());
+		}
+		
+		
 	}
 
 
